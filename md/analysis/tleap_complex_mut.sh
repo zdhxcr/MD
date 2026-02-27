206a8fc5583c1af089a59db827ad2faf99130304
zdhxcr <zhangyuxi2023@126.com>
2026-02-27 18:23:02 +0800

    Create tleap_complex_mut.sh
    得到丙氨酸扫描突变的突变top文件	注意就是：只需将某残基的CA C N原子保留，其他的删除，并将残基名称改为ALA，经过该脚本就可以生成相关top文件

---
+++ md/analysis/tleap_complex_mut.sh
@@ -0,0 +1,62 @@
+#for complex of small mecular and protein(for mmgbsa
+#tleap.in
+#source protein water and little molecular force filed
+
+##amber18
+#export AMBERHOME=/home3/qwang/amber/amber18
+##amber20
+export AMBERHOME=/home3/htlin/amber20
+
+protein=protein.pdb
+protein_mut=protein_mut.pdb
+ligand=lig_h.mol2
+lig_frcmod=lig.frcmod
+
+cat > input_leap <<EOF
+
+
+source leaprc.protein.ff19SB               
+source leaprc.water.opc                 
+source leaprc.gaff                         
+#source little molecular ff file and ion ff
+loadamberparams $lig_frcmod          
+#loadamberparams frcmod.ionsjc_opc   
+
+p = loadpdb $protein                     
+m = loadpdb $protein_mut                     
+l = loadmol2 $ligand             
+c = combine {p l}
+d = combine {m l} 
+
+savepdb p pro.pdb
+savepdb m pro_mut.pdb
+savepdb l lig.pdb
+savepdb c com.pdb
+savepdb d com_mut.pdb
+
+saveamberparm p pro.prmtop pro.inpcrd
+saveamberparm m pro_mut.prmtop pro_mut.inpcrd
+saveamberparm l lig.prmtop lig.inpcrd
+saveamberparm c com.prmtop com.inpcrd
+saveamberparm d com_mut.prmtop com_mut.inpcrd
+
+solvateoct c OPCBOX 12.0
+ 
+charge c
+addions c Cl- 0
+addions c Na+ 0
+
+saveoff l lig.lib
+saveamberparm c out.prmtop out.inpcrd #1
+savepdb c out.pdb                     #1
+saveamberparm d out_mut.prmtop out_mut.inpcrd #1
+savepdb c out_mut.pdb                     #1
+quit
+
+EOF
+
+# Run tleap
+ $AMBERHOME/bin/tleap -f ./input_leap
+#
+ 
+
